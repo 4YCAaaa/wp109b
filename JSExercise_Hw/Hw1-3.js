d2e2e8727c48b3f75e98b0d6fbf195cee12ef55a
4YCAaaa <diane01154s@gmail.com>
2021-06-27 00:32:09 +0800

    4YCAaaa

--- JSExercise_Hw/Hw1-3.js
+++ JSExercise_Hw/Hw1-3.js
@@ -1,7 +1,18 @@
 /*加分題:請寫一個函數 primeBetween(a,b) 可以印出 a..b 之間的質數
 例如 primeBetween(3,15)*/
-function PrimeBetween(a,b) 
-{
-    for(var i=x;i<=y;i++)
-    console.log(i)
+function isPrime(n) {
+    if (n < 2) 
+        return 0;
+    for (var k=2; k * k <= n; k++)
+        if (n % k == 0) 
+            return 0;
+    return 1;
 }
+
+function primeBetween(a, b) {
+    for (var i=a; i<=b; i++)
+        if (isPrime(i))
+        console.log(i);
+}
+
+primeBetween(3, 15)
